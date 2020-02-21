5b0722ba41cef0a81bca0cfc2c66e8d35f77e915
IsaiasGC <j.isaias.g.c@gmail.com>
2020-02-21 13:19:34 -0600

    Login agregado

---
+++ lib/login.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    final txtUserController = TextEditingController();
+    final txtPwdController = TextEditingController();
+    final logo = CircleAvatar(
+      radius: 45.0,
+      child: Image.network("https://dms.com.pe/wp-content/uploads/2017/02/acceso-web.png"),
+    );
+    final txtUser = TextFormField(
+      keyboardType: TextInputType.emailAddress,
+      controller: txtUserController,
+      decoration: InputDecoration(
+        hintText: "Introduce el Usuario",
+        contentPadding: EdgeInsets.fromLTRB(20, 10, 20, 10),
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(45.0))
+      ),
+    );
+    final txtPwd = TextFormField(
+      controller: txtPwdController,
+      obscureText: true,
+      decoration: InputDecoration(
+        hintText: "Introduce el Usuario",
+        contentPadding: EdgeInsets.fromLTRB(20, 10, 20, 10),
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(45.0))
+      ),
+    );
+    return MaterialApp(
+
+      home:  Scaffold(
+//        appBar: AppBar(
+//          title: Text("Log-in"),
+//          backgroundColor: Colors.lightGreen,
+//        ),
+        body: Center(
+          child: ListView(
+            shrinkWrap: true,
+            padding: EdgeInsets.only(left: 24.0, right: 24.0),
+            children: <Widget>[
+              logo,
+              SizedBox(height: 30),
+              txtUser,
+              SizedBox(height: 30),
+              txtPwd,
+              SizedBox(height: 30),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:splashscreen/splashscreen.dart';
+import 'login.dart';
 
 void main()=>runApp(Splash());
 
@@ -14,15 +15,15 @@ class SplashForm extends State<Splash>{
   @override
   Widget build(BuildContext context){
     return MaterialApp(
-      // routes: {
-      //   '/login' : (context)=>Login(),
+      routes: {
+        '/login' : (context)=>Login(),
       //   '/dash' : (context)=>Dash(),
       //   '/course' : (context)=>Course(),
-      // },
+      },
       home: SplashScreen(
         seconds: 3,
         image: Image.network("https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.freepng.es%2Fpng-yq9evx%2F&psig=AOvVaw1RAHHL6NDLi3XhMwNtsjg9&ust=1582216412526000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCOjA28yF3ucCFQAAAAAdAAAAABAI"),
-        // navigateAfterSeconds: Login(),
+        navigateAfterSeconds: Login(),
         title: Text("Bienvenido a ClassITC", 
           style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
         ),
